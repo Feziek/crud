e823b7ed8ffa82d9d742ff41922690bac686c911
jnelbaylen <jnelbaylen@gmail.com>
2026-03-24 09:56:30 +0800

    feat: add root and books API endpoints

--- server/src/main.js
+++ server/src/main.js
@@ -1,6 +1,23 @@
 import express from "express"
 
+import { pool } from "./lib/db.js"
+
 const app = express()
+
+app.get("/", (req, res) => {
+    res.json("Backend is running successfully")
+})
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books"
+    pool.query(q,(err, data) => {
+        if (err) {
+            console.error("DB ERROR:", err)
+            return res.status(500).json(err)
+        } 
+        return res.json(data)
+    }) 
+})
 
 const PORT = process.env.PORT || 8000
 
