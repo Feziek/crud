d4d3c0c3431bb43388b7bfedf26279db588ce357
jnelbaylen <jnelbaylen@gmail.com>
2026-03-24 09:53:22 +0800

    feat: add drop table for books and add initial data

--- db/books.sql
+++ db/books.sql
@@ -1,6 +1,12 @@
+DROP TABLE IF EXISTS books;
+
 CREATE TABLE books (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     description VARCHAR(255) NOT NULL,
     cover VARCHAR(50) NULL
 );
+
+-- INSERT INTO books
+-- VALUES
+--     (1, 'Sample Book', 'This is a test description for the sample book.', 'sample.jpg');
