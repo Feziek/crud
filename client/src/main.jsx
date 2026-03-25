9b1d8009cc92fff44aefc41e8d4847909f411c1d
jnelbaylen <jnelbaylen@gmail.com>
2026-03-25 16:14:44 +0800

    chore: apply alias for styles and rename App to Root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
+import '@styles/index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import { Root } from '@routes/Root.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Root />
+  </StrictMode>
 )
