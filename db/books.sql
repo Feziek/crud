8db2efe0171ccc24b5726ed1863ef5f3503491b3
jnelbaylen <jnelbaylen@gmail.com>
2026-03-26 02:07:56 +0800

    chore: update books table schema and clean up SQL comments

--- db/books.sql
+++ db/books.sql
@@ -4,6 +4,7 @@ CREATE TABLE books (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     description VARCHAR(255) NOT NULL,
+    price DECIMAL (10, 2) NOT NULL,
     cover VARCHAR(50) NULL
 );
 
@@ -11,8 +12,8 @@ CREATE TABLE books (
 -- VALUES
 --     (1, 'Sample Book', 'This is a test description for the sample book.', 'sample.jpg');
 
-ALTER TABLE books
-ADD COLUMN price INT NOT NULL;
+-- ALTER TABLE books
+-- ADD COLUMN price INT NOT NULL;
 
 -- DELETE FROM books 
 -- WHERE id BETWEEN 2 AND 10;
